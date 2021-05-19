2cfb62ce92db1829d599cd27b1b22d22c419c696
Apurba Panja <apurbapanja1@gmail.com>
2021-05-19 16:15:32 +0530

    Added codes which implement Composition

---
+++ OOP/BedroomWall.java
@@ -0,0 +1,10 @@
+public class BedroomWall {
+    private String direction;
+
+    public BedroomWall(String direction) {
+        this.direction = direction;
+    }
+    public String getDirection() {
+        return this.direction;
+    }
+}
