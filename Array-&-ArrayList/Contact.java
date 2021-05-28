ab39740cc781760a30111773edbef60b58d55f19
Apurba Panja <apurbapanja1@gmail.com>
2021-05-28 23:09:12 +0530

    Added code implementation with ArrayList

---
+++ Array-&-ArrayList/Contact.java
@@ -0,0 +1,21 @@
+public class Contact {
+    private String name;
+    private String phoneNo;
+
+    public Contact(String name, String phoneNo) {
+        this.name = name;
+        this.phoneNo = phoneNo;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhoneNo() {
+        return this.phoneNo;
+    }
+
+    public static Contact createContact(String name, String phoneNo) {
+        return new Contact(name, phoneNo);
+    }
+}
---
+++ Array-&-ArrayList/GroceryList.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your grocery list");
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position-1, newItem);
+        System.out.println("Grocery item " + (position) + " has been modified.");
+    }
+
+    public void removeGroceryItem(int position) {
+        String removedItem = groceryList.get(position);
+        groceryList.remove(position - 1);
+    }
+
+    public String findGroceryItem(String searchItem) {
+        boolean exists = groceryList.contains(searchItem);
+        int position = groceryList.indexOf(searchItem);
+        return exists ? (searchItem + " is in position " + position)
+                : (searchItem + " not exists in the list");
+    }
+}
